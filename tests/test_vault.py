c42231d4abb853231f928cdc85f6a863392e511a
daniel salmon <salmond44@gmail.com>
2024-10-25 09:34:35 -0400

    Refactor local vault setup and add tests

--- tests/test_vault.py
+++ tests/test_vault.py
@@ -1,17 +1,18 @@
 import pytest
+from pathlib import Path
 
-from firstpass.vault import MemoryVault, Vault
+from firstpass.vault import LocalVault, MemoryVault, Vault
 
 
 @pytest.fixture()
-def vault():
+def vault() -> Vault:
     return MemoryVault(password="password")
 
 
 @pytest.mark.parametrize(
     "plaintext", [b"plaintext", b"what a string", b'{"pickles": "rule"}']
 )
-def test_encrypt_decrypt(plaintext: bytes, vault: Vault):
+def test_encrypt_decrypt(plaintext: bytes, vault: Vault) -> None:
     assert vault.decrypt(vault.encrypt(plaintext)) == plaintext
 
 
@@ -23,8 +24,22 @@ def test_encrypt_decrypt(plaintext: bytes, vault: Vault):
         ("ajapdfipjwe", "pebpjqefvp92!$T$!))"),
     ],
 )
-def test_set_get_delete(name: str, value: str, vault: Vault):
+def test_set_get_delete(name: str, value: str, vault: Vault) -> None:
     vault.set(name, value)
+    assert vault.get(name) == value
+    vault.delete(name)
+    assert vault.get(name) is None
+
+
+@pytest.mark.parametrize(
+    "name, value",
+    [
+        ("pybites", "super-secret-password"),
+    ],
+)
+def test_local_vault(name: str, value: str, tmp_path: Path) -> None:
+    vault = LocalVault(password="password", file=tmp_path / "secrets")
+    vault.set(name=name, value=value)
     assert vault.get(name) == value
     vault.delete(name)
     assert vault.get(name) is None
