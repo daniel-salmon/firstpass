3bda7d3b6ece83dc84c6f8831ad289508dea4527
daniel salmon <salmond44@gmail.com>
2024-10-31 21:23:33 -0400

    Remove unnecessary type hint

--- app/main.py
+++ app/main.py
@@ -72,7 +72,7 @@ async def get_current_user(
         payload = jwt.decode(
             token, settings.secret_key, algorithms=[settings.jwt_signing_algorithm]
         )
-        username: str = payload.get("sub")
+        username = payload.get("sub")
         if username is None:
             raise credentials_exception
     except InvalidTokenError:
